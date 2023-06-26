5ef24a8d01ca222c57d60cfaa625684b253f4f60
biswadeepc <biswadeep.chakraborty@gmail.com>
2023-06-26 22:21:40 +0530

    First commit

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,84 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
+
+const initialState = [
+  {
+    id: "1",
+    title: "Learning Redux Toolkit",
+    content: "I've heard good things.",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
+  },
+  {
+    id: "2",
+    title: "Slices...",
+    content: "The more I say slice, the more I want pizza.",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
+  },
+];
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      return state;
+    },
+    /* 
+        If you need to customize the creation of the payload value of an action creator by means of a prepare callback, 
+        the value of the appropriate field of the reducers argument object should be an object instead of a function. 
+        
+        This object must contain two properties: reducer and prepare. The value of the reducer field should be the
+        case reducer function while the value of the prepare field should be the prepare callback function:
+        */
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            date: new Date().toISOString(),
+            userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
+          },
+        };
+      },
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+      if (existingPost) {
+        existingPost.reactions[reaction]++;
+      }
+    },
+  },
+});
+
+export const selectAllPosts = (state) => state.posts;
+
+export const { postAdded, reactionAdded } = postsSlice.actions;
+
+export default postsSlice.reducer;
